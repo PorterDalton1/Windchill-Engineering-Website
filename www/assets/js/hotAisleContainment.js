4c5257eb7665a2454a662a5bed4c00bb83bb2f14
PorterDalton <porter.dalton2@gmail.com>
2026-06-19 15:23:53 +0000

    Stop tracking files now in .gitignore

--- www/assets/js/hotAisleContainment.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('#find_out').on('click', function () {
-        $('html,body').animate({
-            scrollTop: $("#second_hot").offset().top
-        }, 'slow');
-    });
-});
--- www/secure/Application.cfc
+++
@@ -1,7 +0,0 @@
-component {
-    this.name="Vendor";
-    this.sessionManagement="true";
-    this.sessionTimeout=createTimeSpan(0,0,10,0);
-	this.defaultdatasource="simpleDB";
-    this.setClientCookies="false";
-}
